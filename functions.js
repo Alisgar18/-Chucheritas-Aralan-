024545be4bbaa17db61e2b1148aa0b2ee910bb4a
Alisgar18 <alisonroblesgar12@gmail.com>
2025-11-27 00:27:09 -0600

    Descripción del cambio

--- functions.js
+++ functions.js
@@ -2,3 +2,17 @@
 function toggleMenu() {
     document.querySelector("nav ul").classList.toggle("active");
 }
+
+function enviarFormulario(event) {
+    event.preventDefault(); // Evita que se recargue la página
+
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    const mensajeDiv = document.getElementById("mensaje-confirmacion");
+    mensajeDiv.innerHTML = `Gracias <b>${nombre}</b>, tu mensaje ha sido enviado correctamente ❤️`;
+    mensajeDiv.style.display = "block";
+
+    document.querySelector(".form-contacto").reset();
+}
